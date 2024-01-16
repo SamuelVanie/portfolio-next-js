24a43af8f49aee4f92dc24633296b49d74fa33f4
SamuelVanie <samuelmichaelvanie@gmail.com>
2024-01-16 22:19:42 +0100

    chore: change project folder organization and avatar

--- src/app/i18n.js
+++ src/app/i18n.js
@@ -1,22 +1,22 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
 
 //import translationEN from './locales/en.json'; // English translations
-import translationFR from './locales/fr.json'; // French translations
+import translationFR from "./locales/fr.json"; // French translations
 
 const resources = {
- /* en: {
+  /* en: {
     translation: translationEN,
   }*/
   fr: {
     translation: translationFR,
-  }
+  },
 };
 
 i18n.use(initReactI18next).init({
   resources,
-  lng: 'en', // Set the default language
-  fallbackLng: 'en', // Fallback language if a translation is missing
+  lng: "fr", // Set the default language
+  fallbackLng: "fr", // Fallback language if a translation is missing
   interpolation: {
     escapeValue: false, // React already escapes the values
   },
